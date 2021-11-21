52efdcbd3748652488a35ad26c712954e83f560b
mdaudev <mdlovingf2021@gmail.com>
2021-11-21 19:00:25 -0300

    basic porfolio info

--- src/config.js
+++ src/config.js
@@ -1,102 +1,96 @@
 // config.js
 module.exports = {
     github: {
-        username: 'arifszn', // Your GitHub org/user name. (Required)
+        username: 'augusto-scarvalho', // Your GitHub org/user name. (Required)
         sortBy: 'stars', // stars | updated
         limit: 8, // How many projects to display.
         exclude: {
-            forks: false, // Forked projects will not be displayed if set to true.
+            forks: true, // Forked projects will not be displayed if set to true.
             projects: [] // These projects will not be displayed. example: ['my-project1', 'my-project2']
         }
     },
     social: {
-        linkedin: 'ariful-alam',
-        twitter: 'arif_swozon',
+        linkedin: 'augustoscarvalho',
+        twitter: '',
         facebook: '',
         dribbble: '',
         behance: '',
         medium: '',
-        devto: '',
-        website: 'https://arifszn.github.io',
+        devto: 'augustoscarvalho',
+        website: 'https://cmmdr.dev',
         phone: '',
-        email: 'contact@arifszn.com'
+        email: 'ct.cmmdr@gmail.com'
     },
     skills: [
-        'PHP',
-        'Laravel',
+        'Python',
+        'Node.js',
         'JavaScript',
         'React.js',
-        'Vue.js',
-        'Node.js',
-        'Jquery',
-        'MySQL',
-        'Git',
-        'Docker',
-        'CSS',
-        'Antd',
-        'Tailwind',
-        'Bootstrap',
+        'Blockchain',
+        'Solidity',
+        'NLP',
+        'Chatbots',
     ],
     experiences: [
-        { 
-            company: 'Monstarlab Bangladesh',
-            position: 'Backend Engineer II',
-            from: 'September 2021',
+        {
+            company: 'Freelancer',
+            position: 'DApp Developer',
+            from: 'June 2020',
             to: 'Present'
         },
-        { 
-            company: 'Orangetoolz',
-            position: 'Jr. Full Stack Engineer',
-            from: 'July 2019',
-            to: 'August 2021'
+        {
+            company: 'Banco do Brasil',
+            position: 'Chatbot Developer',
+            from: 'April 2017',
+            to: 'Present'
         },
-        { 
-            company: 'Techvillage',
-            position: 'Jr. Software Engineer',
-            from: 'January 2019',
-            to: ' June 2019'
+        {
+            company: 'Banco do Brasil',
+            position: 'Social Media Analyst',
+            from: 'December 2014',
+            to: 'April 2017'
+        },
+        {
+            company: 'Banco do Brasil',
+            position: 'Customer Service Analyst',
+            from: 'December 2014',
+            to: 'September 2014'
         }
     ],
     education: [
         { 
-            institution: 'American International University-Bangladesh',
-            degree: 'Bachelor of Science',
-            from: '2015',
-            to: '2019'
+            institution: 'Universidade Paulista',
+            degree: 'Bachelor of Computer Science',
+            from: '2016',
+            to: '2020'
         },
         { 
-            institution: 'Cantonment College, Jessore',
-            degree: 'Higher Secondary Certificate (HSC)',
-            from: '2012',
-            to: '2014',
-        },
-        { 
-            institution: 'Chowgacha Shahadat Pilot High School',
-            degree: 'Secondary School Certificate (SSC)',
-            from: '2007',
-            to: '2012'
+            institution: 'Universidade de São Paulo',
+            degree: 'Licentiate of Physics',
+            from: '2009',
+            to: 'unfinished',
         }
     ],
     blog: {
         // Display blog posts from your medium or dev.to account. (Optional)
         source: 'dev.to', // medium | dev.to
-        username: 'arifszn',
+        username: 'augustoscarvalho',
         limit: 2 // How many posts to display. Max is 10.
     },
     googleAnalytics: {
         // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-        id: 'G-WLLB5E14M6' // Please remove this and use your own tag id
+        id: '' // Please remove this and use your own tag id
     },
     hotjar: {
-        id: '2617601', //  Please remove this and use your own id
+        id: '', //  Please remove this and use your own id
         snippetVersion : 6
     },
     themeConfig: {
-        default: 'light',
+        default: 'forest',
 
         // Hides the switch in the navbar
         // Useful if you want to support a single color mode
-        disableSwitch: false,
+        disableSwitch: true,
 
         // Should we use the prefers-color-scheme media-query,
         // using user system preferences, instead of the hardcoded default
